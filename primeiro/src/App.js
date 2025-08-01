ba33853c6f30118735776073f40361c4b6a3e2f7
VitorEduardo7 <vitor.virginio@etec.sp.gov.brm>
2025-08-01 18:13:40 -0300

    Envio Inicial

---
+++ primeiro/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Meu app</h1>
+      <p>gwguwigwubiowuiogwui</p>
+      </div>
+  );
+}
+
+export default App;
