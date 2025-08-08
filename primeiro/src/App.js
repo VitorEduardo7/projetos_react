e272f6132bceffecb1c1d7030430e5f2daea138a
VitorEduardo7 <vitor.virginio@etec.sp.gov.br>
2025-08-08 17:20:41 -0300

    primeiro

--- primeiro/src/App.js
+++ primeiro/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import Frase from './components/Frase';
+import DigaMeuNome from './components/DigaMeuNome';
+
 
 function App() {
   return (
     <div className="App">
-      <h1> Meu app</h1>
-      <p>gwguwigwubiowuiogwui</p>
-      </div>
+      <h1> Meu app react</h1>
+      <HelloWorld />
+      <Frase />
+      <DigaMeuNome nome="Vitor" />
+
+    </div>
   );
 }
 
